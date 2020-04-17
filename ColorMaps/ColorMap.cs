1c4acf8396367b8c738b1f7b1b700864925d4540
Denis Lambolez <denis.lambolez@gmail.com>
2020-04-17 19:20:14 +0200

    Work in progress

--- ColorMaps/ColorMap.cs
+++ ColorMaps/ColorMap.cs
@@ -2,7 +2,6 @@
 using System.Collections.ObjectModel;
 using Windows.UI;
 using Windows.ApplicationModel.Resources;
-using Windows.Graphics.DirectX;
 
 namespace CatsHelpers.ColorMaps
 {
@@ -13,14 +12,11 @@ namespace CatsHelpers.ColorMaps
     /// </summary>
     public class ColorMap
     {
-        private const int MAX_COUNT_COLORDATA = 256;
-        private const int MAX_BYTE_COLOR = 255;
-
         // Get resource loader for the library
         private readonly ResourceLoader resourceLoader = ResourceLoader.GetForCurrentView("CatsHelpers/ErrorMessages");
         // Backing store for the color data
         private readonly (double, double, double)[] _colorData;
-
+        private readonly int maxIndexColorData;
 
         /// <summary>
         /// Get color data as a read only collection of truples of double (sRGB) 
@@ -31,13 +27,11 @@ namespace CatsHelpers.ColorMaps
         /// <summary>
         /// Create the color scale from color data
         /// </summary>
-        /// <param name="colorData">An array of 256 truples of double, with each component being between 0 and 1</param>
+        /// <param name="colorData">An array of truples of double, with each component being between 0 and 1</param>
         /// <exception cref="ArgumentNullException">colorData can't be null</exception>
-        /// <exception cref="ArgumentOutOfRangeException">colorData must have exactly 256 elements, with all elements components being between 0 and 1</exception>
         public ColorMap((double, double, double)[] colorData)
         {
             if (colorData == null) throw new ArgumentNullException(nameof(colorData));
-            if (colorData.Length != MAX_COUNT_COLORDATA) throw new ArgumentOutOfRangeException(nameof(colorData), resourceLoader.GetString("WrongColorDataArraySize"));
             foreach ((double, double, double) data in colorData)
             {
                 if (data.Item1 < 0 || data.Item1 > 1
@@ -49,32 +43,32 @@ namespace CatsHelpers.ColorMaps
             }
 
             _colorData = colorData;
+            maxIndexColorData = _colorData.GetUpperBound(0);
         }
 
         /// <summary>
-        /// Get the color corresponding to the given position on the scale through interpolation 
+        /// Get the sRGB data (truples of double) corresponding to the given position on the colormap through interpolation 
         /// </summary>
         /// <param name="position">The position on the scale</param>
-        /// <param name="inverse">If true, return the <see cref="Color"/> corresponding to the inversed color scale (Optional ; Default : false)</param>
+        /// <param name="inverse">If true, return the sRGB data corresponding to the inversed colormap (Optional ; Default : false)</param>
         /// <remarks><paramref name="position"/> is a percentage and must be between 0 and 1.</remarks>
         /// <exception cref="ArgumentOutOfRangeException"><paramref name="position"/> Must be in the [0 , 1] range</exception>
-        /// <returns>The color</returns>
-        public Color GetInterpolatedColor(double position, bool inverse = false)
+        /// <returns>The sRGB data as a truples of double</returns>
+        public (double, double, double) GetInterpolatedsRGB(double position, bool inverse = false)
         {
             if (position < 0 || position > 1) throw new ArgumentOutOfRangeException(nameof(position), resourceLoader.GetString("ValueNotPercentage"));
 
             // Inverse the position if needed
             if (inverse) position = 1 - position;
-            // Return the last color if we are at the end of the scale
-            if (position == 1) return SRGBToColor(_colorData[255]); 
 
             // Get the colors just before and after the position
-            double index = position * (MAX_COUNT_COLORDATA - 1);
-            double lowIndex = Math.Truncate(index);
-            double highIndex = lowIndex + 1;
+            double index = position * maxIndexColorData;
+            int lowIndex = (int)Math.Truncate(index);
+            int highIndex = lowIndex + 1;
 
-            (double lowR, double lowG, double lowB) = _colorData[Convert.ToInt32(lowIndex)];
-            (double highR, double highG, double highB) = _colorData[Convert.ToInt32(highIndex)];
+            // Get sRGB data for low and high color
+            var (lowR, lowG, lowB) = _colorData[lowIndex];
+            var (highR, highG, highB) = _colorData[highIndex];
 
             // Linear interpolation of the components values
             double factor = index - lowIndex;
@@ -83,39 +77,92 @@ namespace CatsHelpers.ColorMaps
             double newB = lowB + (highB - lowB) * factor;
 
             // Return fully opaque color
-            return SRGBToColor(newR, newG, newB);
+            return (newR, newG, newB);
         }
 
-        //public Color[] GetIndexedColorMap(int count)
-        //{
-        //    var colorMap = new Color[count];
+        /// <summary>
+        /// Get the <see cref="Color"/> corresponding to the given position on the colormap through interpolation 
+        /// </summary>
+        /// <param name="position">The position on the scale</param>
+        /// <param name="inverse">If true, return the <see cref="Color"/> corresponding to the inversed color scale (Optional ; Default : false)</param>
+        /// <remarks><paramref name="position"/> is a percentage and must be between 0 and 1.</remarks>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="position"/> Must be in the [0 , 1] range</exception>
+        /// <returns><see cref="Color"/> The color</returns>
+        public Color GetInterpolatedColor(double position, bool inverse = false)
+        {
+            var (r, g, b) = GetInterpolatedsRGB(position, inverse);
+            
+            // Return fully opaque color
+            return ColorFromsRGB(r, g, b);
+        }
 
-        //    // Expansion
-        //    if (count > MAX_COUNT_COLORDATA)
-        //    {
+        /// <summary>
+        /// Create an indexed colormap, as an array of bytes of bytes (4 bytes by color), by interpolating colors data
+        /// </summary>
+        /// <param name="count">Number of colors in the new colormap</param>
+        /// <returns>The array of byte of byte (4 bytes per color)</returns>
+        public byte[][] CreateIndexedBytesColorMap(int count)
+        {
+            if (count < 1) throw new ArgumentOutOfRangeException(nameof(count), resourceLoader.GetString("ValueNotStrictlyPositive"));
+            
+            var colorMap = new byte[count][];
+            double position;
 
-        //    }
-        //    // Compression
-        //    else if (count < MAX_COUNT_COLORDATA)
-        //    {
+            for (int index = 0; index < count; index++)
+            {
+                position = (double)index / count;
+                
+                var (r, g, b) = GetInterpolatedsRGB(position);
+                colorMap[index] = BytesFromsRGB(r, g, b);
+            }
 
-        //    }
-        //    else
-        //    {
-        //        for (int index = 0; index < count; index++) colorMap[index] = SRGBToColor(_colorData[index]);
-        //    }
-        //}
+            return colorMap;
+        }
+
+        /// <summary>
+        /// Create an indexed colormap, as an array of <see cref="Color"/>, by interpolating colors data
+        /// </summary>
+        /// <param name="count">Number of colors in the new colormap</param>
+        /// <returns>The array of <see cref="Color"/></returns>
+        public Color[] CreateIndexedColorsColorMap(int count)
+        {
+            if (count < 1) throw new ArgumentOutOfRangeException(nameof(count), resourceLoader.GetString("ValueNotStrictlyPositive"));
+
+            var colorMap = new Color[count];
+            double position;
+
+            for (int index = 0; index < count; index++)
+            {
+                position = (double)index / count;
+
+                var (r, g, b) = GetInterpolatedsRGB(position);
+                colorMap[index] = ColorFromsRGB(r, g, b);
+            }
+
+            return colorMap;
+        }
+
+        // Convert sRGB components to bytes array (4 bytes)
+        private static byte[] BytesFromsRGB(double r, double g, double b)
+        {
+            var color = new byte[4];
+
+            // Fully opaque color
+            color[0] = byte.MaxValue;
+            color[1] = Convert.ToByte(r * byte.MaxValue);
+            color[2] = Convert.ToByte(g * byte.MaxValue);
+            color[3] = Convert.ToByte(b * byte.MaxValue);
+
+            return color;
+        }
 
         // Convert sRGB components to Color struct
-        private static Color SRGBToColor(double r, double g, double b)
+        private static Color ColorFromsRGB(double r, double g, double b)
         {
-            return Color.FromArgb(255, Convert.ToByte(r * MAX_BYTE_COLOR), Convert.ToByte(g * MAX_BYTE_COLOR), Convert.ToByte(b * MAX_BYTE_COLOR));
-        }
+            byte[] color = BytesFromsRGB(r, g, b);
 
-        // Convert sRGB truples to Color struct
-        private static Color SRGBToColor((double, double, double) srgb)
-        {
-            return Color.FromArgb(255, Convert.ToByte(srgb.Item1 * MAX_BYTE_COLOR), Convert.ToByte(srgb.Item2 * MAX_BYTE_COLOR), Convert.ToByte(srgb.Item3 * MAX_BYTE_COLOR));
+            // Return fully opaque color
+            return Color.FromArgb(color[0], color[1], color[2], color[3]);
         }
     }
 #pragma warning restore CS0419 // La référence de l'attribut cref est ambiguë
